3cdd834354e1db8a3cb104e682ea911229039767
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-05-25 18:42:26 +0300

    added comments to script

--- lesson17_step2(маркування).py
+++ lesson17_step2(маркування).py
@@ -24,3 +24,12 @@ class TestMainPage1():
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
+
+# Чтобы запустить все тесты, не имеющие заданную маркировку, можно использовать инверсию.
+# Для запуска всех тестов, не отмеченных как smoke, нужно выполнить команду:
+#
+# pytest -s -v -m "not smoke" test_fixture8.py
+#
+# Для запуска тестов с разными метками можно использовать логическое ИЛИ.
+# Запустим smoke и regression-тесты:
+# pytest -s -v -m "smoke or regression" test_fixture8.py
