6d8a269d05a3f792e5f45ab0aa33638ab9e37385
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-05-26 15:08:49 +0300

    task with parametrize

---
+++ lesson18_step3.py
@@ -0,0 +1,32 @@
+import time
+import math
+import pytest
+from selenium import webdriver
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(5)
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+@pytest.mark.parametrize('link', ["https://stepik.org/lesson/236895/step/1",
+                                  "https://stepik.org/lesson/236896/step/1",
+                                  "https://stepik.org/lesson/236897/step/1",
+                                  "https://stepik.org/lesson/236898/step/1",
+                                  "https://stepik.org/lesson/236899/step/1",
+                                  "https://stepik.org/lesson/236903/step/1",
+                                  "https://stepik.org/lesson/236904/step/1",
+                                  "https://stepik.org/lesson/236905/step/1"])
+def test_guest_should_see_login_link(browser, link):
+    browser.get(f'{link}')
+    answer = str(math.log(int(time.time())))
+    browser.find_element_by_tag_name("textarea").send_keys(answer)
+    browser.find_element_by_css_selector('button.submit-submission').click()
+    time.sleep(1)
+    msg = browser.find_element_by_css_selector('pre.smart-hints__hint')
+    assert "Correct!" == msg.text, f"Is {msg.text} not 'Correct!'"
