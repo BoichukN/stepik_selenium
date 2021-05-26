afdd9fdd0c6e9fd38e88b02865b388d15c4ae795
Natalia Boichuk <boichuk.n.m@gmail.com>
2021-05-26 18:27:32 +0300

    add_experimental_options

--- cross_browser/conftest.py
+++ cross_browser/conftest.py
@@ -1,5 +1,5 @@
 import time
-
+from selenium.webdriver.chrome.options import Options
 import pytest
 from selenium import webdriver
 
@@ -7,18 +7,25 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default=None,
                      help="Choose browser: chrome or firefox")
+    parser.addoption('--languages', action='store', default=None,
+                     help="Choose language: ru, en, ua")
 
 
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
+    user_language = request.config.getoption("languages")
     browser = None
     if browser_name == "chrome":
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", user_language)
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        browser = webdriver.Firefox(firefox_profile=fp)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
